aa47ead1015a9db3a3f8a7eb1979197823a68b5b
Bellinda12 <bellindayin@gmail.com>
2021-07-29 15:40:08 -0700

    added python scripts for module

---
+++ analysis/Bellinda/scripts/project_functions.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from functools import reduce
+import seaborn as sns
+from matplotlib import pyplot
+
+def hap_load_and_process(url_or_path_to_csv_file, rename_dict,final_list):
+
+    # Method Chain 1 (Load data and deal with missing data)
+
+    df1 = (
+          pd.read_csv(url_or_path_to_csv_file)
+          .rename(columns=rename_dict)
+          #.dropna()
+          # etc...
+      )
+
+    # Method Chain 2 (Create new columns, drop others, and do processing)
+
+    df2 = (
+          df1
+          #.assign(status=lambda x: np.where((x.period > 2014), 1, 0))
+          .sort_values("country", ascending=True)
+          .reset_index(drop=True)
+          .loc[:, final_list]
+      )
+
+    # Make sure to return the latest dataframe
+
+    return df2 
+
+def ind_load_and_process(url_or_path_to_csv_file):
+
+    # Method Chain 1 (Load data and deal with missing data)
+
+    df1 = (
+          pd.read_csv(url_or_path_to_csv_file)
+          .rename(columns={"Location":"country","Period":"period","Value":"value"})
+          #.dropna()
+          # etc...
+      )
+
+    # Method Chain 2 (Create new columns, drop others, and do processing)
+
+    df2 = (
+          df1
+          .assign(status=lambda x: np.where((x.period > 2014), 1, 0))
+          .sort_values("country", ascending=True)
+          .reset_index(drop=True)
+          .loc[:, ["country", "period", "value",'status']]
+      )
+
+    # Make sure to return the latest dataframe
+
+    return df2 
+    
+def filter_years(df):
+    #df['status'] = df.apply(keep_or_discard,axis='columns')
+    df=df.loc[df['status'] == 1]
+    df=df.drop(columns=['status'])
+    return df
+
+def edit_data(dataFrame):
+    
+    numbs=list(dataFrame[list(dataFrame.columns)[2]])
+    for string in numbs:
+        loc = string.find(' ')
+        numbs[numbs.index(string)]=string[0:loc]
+
+    return numbs
+
+def clean_ind(df):
+    
+    df=filter_years(df)
+    df[[list(df.columns)[2]]]=edit_data(df)
+    
+    return df
---
+++ analysis/Bellinda/scripts/vari.py
@@ -0,0 +1,29 @@
+#file paths to use 
+h2015_data="../../data/raw/2015.csv"
+h2016_data="../../data/raw/2016.csv"
+h2017_data="../../data/raw/2017.csv"
+h2018_data="../../data/raw/2018.csv"
+h2019_data="../../data/raw/2019.csv"
+
+#Health Indicators
+alco="../../data/raw/Alcohol_consumption_2015-2019.csv"
+homi="../../data/raw/homicide_2015-2019.csv"
+road="../../data/raw/road_traffic_incidence_2015-2019.csv"
+smoke="../../data/raw/smoking_2015-2018.csv"
+suic="../../data/raw/suicide_rates_2015-2019.csv"
+
+#variables:
+dict_2015={"Country": "country", "Happiness Score": "2015", "Health (Life Expectancy)": "life 2015"}
+list_2015=["country", "2015", "life 2015"]      
+
+dict_2016={"Country": "country", "Happiness Score": "2016", "Health (Life Expectancy)": "life 2016"}
+list_2016=["country", "2016", "life 2016"] 
+
+dict_2017={"Country": "country", "Happiness.Score": "2017", "Health..Life.Expectancy.": "life 2017"}
+list_2017=["country", "2017", "life 2017"] 
+
+dict_2018={"Country or region": "country", "Score": "2018", "Healthy life expectancy": "life 2018"}
+list_2018=["country", "2018", "life 2018"] 
+
+dict_2019={"Country or region": "country", "Score": "2019", "Healthy life expectancy": "life 2019"}
+list_2019=["country", "2019", "life 2019"]
